2571e750024f503830631af975fbd08a72343966
Ka Tam <nkltam2003@gmail.com>
2017-10-26 10:47:48 -0700

    add date and number mask directive

---
+++ directive/validDate.js
@@ -0,0 +1,68 @@
+﻿/*
+    Valid Date directive 
+    binds to the input control along side with ngModel, and apply a filter mask to the input
+    to ensure only valid date formatted value (YYYY/MM/DD) can be entered
+*/
+
+'use strict';
+
+ var app = angular.module('myApp', []);
+
+    app.controller('myController', function($scope) {
+    });
+
+    app.directive('validDate', function () {
+        return {
+            require: '?ngModel',
+            link: function ($scope, element, attrs, ngModelCtrl) {
+                if (!ngModelCtrl) {
+                    return;
+                }
+
+                ngModelCtrl.$parsers.push(function (val) {
+                    if (angular.isUndefined(val)) {
+                        var val = '';
+                    }
+
+                    var clean = val.replace(/[a-zA-Z\+\-\*\&\^\%\$\#\@\!\~\|\{\}\[\]\(\)\:\;\"\'\<\>\,\.\?]+/g, '');
+                    var yearMonthDayCheck = null;
+
+                    if (clean.indexOf('-') > -1) {
+                        yearMonthDayCheck = clean.split('-');
+                    } else {
+                        yearMonthDayCheck = [clean];
+                    }
+                    var numberOfPartsEntered = (yearMonthDayCheck) ? yearMonthDayCheck.length : 0;
+
+                    switch (numberOfPartsEntered) {
+                        case 1:
+                            clean = yearMonthDayCheck[0].substring(0, 4);
+                            break;
+                        case 2:
+                            clean = yearMonthDayCheck[0].substring(0, 4) + '-';
+                            clean += yearMonthDayCheck[1].substring(0, 2);
+                            break;
+                        case 3:
+                            clean = yearMonthDayCheck[0].substring(0, 4) + '-';
+                            clean += yearMonthDayCheck[1].substring(0, 2) + '-';
+                            clean += yearMonthDayCheck[2].substring(0, 2);
+                            break;
+                        default: 
+                            break;
+                    }
+
+                    if (val !== clean) {
+                        ngModelCtrl.$setViewValue(clean);
+                        ngModelCtrl.$render();
+                    }
+                    return clean;
+                });
+
+                element.bind('keypress', function (event) {
+                    if (event.keyCode === 32) {
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+    }
---
+++ directive/validNumber.js
@@ -0,0 +1,58 @@
+﻿/*
+    Valid Price directive 
+    binds to the input control along side with ngModel, and apply a filter mask to the input
+    to ensure only numerical value with 2 decimal places can be entered
+*/
+
+'use strict';
+
+ var app = angular.module('myApp', []);
+
+    app.controller('myController', function($scope) {
+    });
+
+    app.directive('validNumber', function () {
+        return {
+            require: '?ngModel',
+            link: function ($scope, element, attrs, ngModelCtrl) {
+                if (!ngModelCtrl) {
+                    return;
+                }
+
+                ngModelCtrl.$parsers.push(function (val) {
+                    if (angular.isUndefined(val)) {
+                        var val = '';
+                    }
+
+                    var clean = val.replace(/[^-0-9\.]/g, '');
+                    var negativeCheck = clean.split('-');
+                    var decimalCheck = clean.split('.');
+                    if (!angular.isUndefined(negativeCheck[1])) {
+                        negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+                        clean = negativeCheck[0] + '-' + negativeCheck[1];
+                        if (negativeCheck[0].length > 0) {
+                            clean = negativeCheck[0];
+                        }
+
+                    }
+
+                    if (!angular.isUndefined(decimalCheck[1])) {
+                        decimalCheck[1] = decimalCheck[1].slice(0, 2);
+                        clean = decimalCheck[0] + '.' + decimalCheck[1];
+                    }
+
+                    if (val !== clean) {
+                        ngModelCtrl.$setViewValue(clean);
+                        ngModelCtrl.$render();
+                    }
+                    return clean;
+                });
+
+                element.bind('keypress', function (event) {
+                    if (event.keyCode === 32) {
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+    }
